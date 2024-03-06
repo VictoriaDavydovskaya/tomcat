e2eb6f5d42dd4404fbe56d15a0dae9db156d0170
VD_ <victoria.davydovskaya@gmail.com>
2024-03-06 18:12:39 +0100

    just list

--- src/main/java/com/andersen/servlets/ListServlet.java
+++ src/main/java/com/andersen/servlets/ListServlet.java
@@ -7,18 +7,17 @@ package com.andersen.servlets;
         import jakarta.servlet.http.HttpServletResponse;
 
         import java.io.IOException;
-        import java.io.PrintWriter;
         import java.util.List;
         import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ListServlet extends HttpServlet {
 
-    private final static String index = "index.jsp";
+    private final static String index = "test_list.jsp";
 
     private List<User> users;
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         users = new CopyOnWriteArrayList<>();
         users.add(new User("Sam", "Smith", 10));
         users.add(new User("John", "Cooper",  37));
@@ -29,12 +28,7 @@ public class ListServlet extends HttpServlet {
             throws ServletException, IOException {
 
         //req.setAttribute("users", users);
-        //req.getRequestDispatcher(index).forward(req, resp);
-        PrintWriter writer = resp.getWriter();
-        for (User el: users) {
-            writer.println(el.getName() + " " +
-                    el.getSurname() + " " + el.getAge());
-        }
+        req.getRequestDispatcher(index).forward(req, resp);
     }
 
 }
